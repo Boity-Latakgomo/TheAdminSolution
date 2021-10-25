7a3945b2823ca3e0c9168fcaedaf791626b5da36
BOITUMELO LATAKGOMO <201736639@keyaka.ul.ac.za>
2021-10-25 22:24:54 +0200

    Add project files.

---
+++ TheAdminSolution/TheAdminSolution/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Acr.UserDialogs;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TheAdminSolution.ViewModels
+{
+    public class LoginPageViewModel : BindableBase
+    {
+        INavigationService _navigationService;
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DelegateCommand LoginCommand { get; }
+        public LoginPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            LoginCommand = new DelegateCommand(OnLogin);
+        }
+        private async void OnLogin()
+        {
+            if (string.IsNullOrEmpty(Username) || string.IsNullOrEmpty(Password))
+            {
+                UserDialogs.Instance.Toast("Fill all the fields");
+            }
+            else if (Username == "AuthAdmin" && Password == "123456")
+            {
+                await _navigationService.NavigateAsync("MainPage");
+            }
+            else
+            {
+                UserDialogs.Instance.Toast("Incorrect username or password");
+            }
+        }
+    }
+}
---
+++ TheAdminSolution/TheAdminSolution/Views/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿
+namespace TheAdminSolution.Views
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
